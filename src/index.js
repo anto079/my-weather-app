ab354eff73ab4211616c909ed46fea6f6db7fc07
anto079 <antonellacestaincani@gmail.com>
2024-06-02 18:13:48 +0200

    Updated the search functionality adding the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function updateWeather(response) {
+  let updatedTemp = document.querySelector(".current-temp-number");
+  let currentTemp = Math.round(response.data.temperature.current);
+  updatedTemp.innerHTML = currentTemp;
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "2t553f9381106a3576b3bf8c8coa7bc4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.getElementById("enter-city");
 
-  let cityElement = document.querySelector("h1");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", changeCity);
+searchCity("Cottbus");
