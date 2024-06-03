01c68309517309bb8b39961cc27a03b6d8e042fc
anto079 <antonellacestaincani@gmail.com>
2024-06-03 15:52:57 +0200

    Added the function to inject the forecast html from Javascript

--- src/index.js
+++ src/index.js
@@ -53,6 +53,29 @@ function changeCity(event) {
 
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15º</strong>
+      </div>
+      <div class="weather-forecast-temperature">9º</div>
+    </div>
+   </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", changeCity);
 searchCity("Cottbus");
+displayForecast();
